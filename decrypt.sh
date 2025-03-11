d0dcf9f98110c6bec4b374fe58d3b8c2ed695091
Ron Moerman <ron@crazyelectron.io>
2025-03-11 20:53:45 +0100

    Update Postgres hostname
    Signed-off-by: Ron Moerman <ron@crazyelectron.io>

--- decrypt.sh
+++ decrypt.sh
@@ -1,6 +1,10 @@
 #1/bin/bash
 
-SOPS_AGE_KEY_FILE=/root/.config/sops/age/keys.txt
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    SOPS_AGE_KEY_FILE=/root/.config/sops/age/keys.txt
+else
+    SOPS_AGE_KEY_FILE=~/.sops/key.txt
+fi
 
 # decrypt HomeAssistant configuration file
 sops -d ./config/configuration.sops.yaml > ./config/configuration.yaml
